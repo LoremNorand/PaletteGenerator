122661b0765cda0be98fa1a6085363dfc45cf75d
LoremNorandStudent <1227197@mtp.by>
2024-12-09 15:30:44 +0300

    UI[Form.cs]: Started dev Main form

--- PaletteGenerator/Form1.cs
+++ PaletteGenerator/Form1.cs
@@ -1,13 +1,10 @@
-﻿using PaletteGenerator.Core;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using PaletteGenerator.Core;
+using PaletteGenerator.Core.Palettes;
+using PaletteGenerator.Algorithms;
+using PaletteGenerator.Core.Colors;
 
 namespace PaletteGenerator
 {
@@ -16,34 +13,16 @@ namespace PaletteGenerator
         public Form1()
         {
             InitializeComponent();
-            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void tabPage1_Click(object sender, EventArgs e)
         {
-            FullColor low = new FullColor();
-            FullColor mid = new FullColor();
-            FullColor top = new FullColor();
 
-            Algorithms.Temp.GenerateLowColor(low);
-            Algorithms.Temp.GenerateMidColor(mid);
-            Algorithms.Temp.GenerateTopColor(top);
-
-            panel1.BackColor = Color.FromArgb(low.RGB.R, low.RGB.G, low.RGB.B);
-            panel2.BackColor = Color.FromArgb(mid.RGB.R, mid.RGB.G, mid.RGB.B);
-            panel3.BackColor = Color.FromArgb(top.RGB.R, top.RGB.G, top.RGB.B);
-
-            label1.Text = Core.Utility.VisualLuminance.Contrast(mid, low).ToString("F2");
-            label2.Text = Core.Utility.VisualLuminance.Contrast(top, low).ToString("F2");
-            label3.Text = Core.Utility.VisualLuminance.Contrast(top, mid).ToString("F2");
-
-            label4.Text = low.HEX;
-            label5.Text = mid.HEX;
-            label6.Text = top.HEX;
         }
     }
 }
